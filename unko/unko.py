f312bcffb6fc0b888fdf5e591c12e537a844665b
RyoyaKawano <rheye29@gmail.com>
2024-03-14 16:06:58 +0900

    f

--- unko/unko.py
+++ unko/unko.py
@@ -3,6 +3,8 @@ import webbrowser
 def browser(url):
     webbrowser.open(url, new=0, autoraise=True)
 
-def printf(contents):
-    import numpy as np
+def printf(contents):    
     print(contents)
+
+def scanf(contents):    
+    input(contents)
